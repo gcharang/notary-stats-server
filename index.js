d7a4fe3c1b6a16b5dcb3bbf8395d908c7be5c0af
gcharang <mrgcharang@gmail.com>
2020-03-17 14:25:58 +0530

    v0.000497

--- index.js
+++ index.js
@@ -292,9 +292,9 @@ const processSmartChain = async (name, start) => {
         let chainName = Object.keys(chain)[0]
         let start = chain[chainName]
         await processSmartChain(chainName, start)
-        notaryData.forEach((notary, index, srcArray) => {
+        notaryData.forEach(function (notary, index, srcArray) {
             const timeStampLastNota = moment(parseInt(notary[`last${chainName}NotaTxnIdStamp`].split(",")[1]))
-            notary[`timeSince${chainName}`] = moment.duration(timeStampLastNota.diff(moment().unix())).humanize(true)
+            // notary[`timeSince${chainName}`] = moment.duration(timeStampLastNota.diff(moment().unix())).humanize(true)
             srcArray[index][`timeSince${chainName}`] = moment.duration(timeStampLastNota.diff(moment().unix())).humanize(true)
             console.log(moment.duration(timeStampLastNota.diff(moment().unix())).humanize(true))
             console.log(`srcArray[index][----]: ${srcArray[index][`timeSince${chainName}`]}`)
